f864d99536981490ca11ed834ce79a51305e7a52
vique1206 <vique1206@gmail.com>
2025-10-08 22:43:09 +0300

    first commit

--- test.py
+++
@@ -1,8 +0,0 @@
-from database.models import *
-from database import engine, Base, get_session
-from services.clients import ClientService
-import database.crud
-import json
-
-boba = int
-print(boba)
